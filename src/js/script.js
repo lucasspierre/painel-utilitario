79b2179f233ff32a2e8536e79759c6129dde3dd3
Lucas Pierre <36829699+lucasspierre@users.noreply.github.com>
2022-03-24 16:31:58 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -8,11 +8,24 @@ function print_page() {
 }
 
 function show() {
-    var str = document.getElementById('nomearquiv').value
-    var titulo = str.replace(/\s+/g, '-').toLowerCase();
-    document.title = titulo;
 
-    document.querySelector("[name='diretorio']").value = '<!--Gerar PDF--><a href="https://www.gov.br/pt-br/campanhas/nosso-brasil/saiba-tudo/documentos/' + titulo + '.pdf" download target="_blank" title="Baixar PDF"><span class="fas fa-print" style="font-size: 22px; color: #2980F2; padding: 5px"> </span></a>'
+    var str = document.getElementById('nomearquiv').value
+
+    str = str.replace(/^\s+|\s+$/g, ''); // remover espaco do comeco e do fim
+    str = str.toLowerCase();
+    
+    // remover acentuacao
+    var from = "ãàáäâèéëêìíïîòóöõôùúüûñç·/_,:;";
+    var to   = "aaaaaeeeeiiiiooooouuuunc------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+      str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+  
+    str = str.replace(/[^a-z0-9 -]/g, '') // remover caracteres inválidos
+      .replace(/\s+/g, '-') // Remover espaços
+
+    document.title = str;
+    document.querySelector("[name='diretorio']").value = '<!--Gerar PDF--><a href="https://www.gov.br/pt-br/campanhas/nosso-brasil/saiba-tudo/documentos/' + str + '.pdf" download target="_blank" title="Baixar PDF"><span class="fas fa-print" style="font-size: 22px; color: #2980F2; padding: 5px"> </span></a>'
 
     var url = document.getElementById("urlimagem").value;
     var conteudo = document.getElementById("conteudo").value;
